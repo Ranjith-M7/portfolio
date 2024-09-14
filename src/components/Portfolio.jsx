ed2ddcff71a54afcfc6c1cb8b1202920baf1368e
Ranjith-M7 <ranjith.mathiyazhagan@gmail.com>
2024-09-14 19:02:14 +0530

    updated

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -4,6 +4,7 @@ import installNode from "../assets/portfolio/installNode.jpg";
 import navbar from "../assets/portfolio/navbar.jpg";
 import reactParallax from "../assets/portfolio/reactParallax.jpg";
 import reactSmooth from "../assets/portfolio/reactSmooth.jpg";
+
 import reactWeather from "../assets/portfolio/reactWeather.jpg";
 
 import { FaExternalLinkAlt } from "react-icons/fa";
